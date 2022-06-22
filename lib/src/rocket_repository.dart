abf798d624d75b34b758923fdbfcadaf4f7e9d7d
Yong Shean <yshean@gmail.com>
2022-06-22 21:52:25 +0800

    initial commit
    very_good create rocket_repository -t dart_pkg --desc "A Dart class which exposes methods to implement rocket-related functionality."

---
+++ lib/src/rocket_repository.dart
@@ -0,0 +1,7 @@
+/// {@template rocket_repository}
+/// A Dart class which exposes methods to implement rocket-related functionality.
+/// {@endtemplate}
+class RocketRepository {
+  /// {@macro rocket_repository}
+  const RocketRepository();
+}
---
+++ test/src/rocket_repository_test.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:rocket_repository/rocket_repository.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('RocketRepository', () {
+    test('can be instantiated', () {
+      expect(RocketRepository(), isNotNull);
+    });
+  });
+}
